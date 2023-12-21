d4d76e84ba225e989d12d029a9256c72dda04ba3
ledrose <embre@bk.ru>
2023-12-21 18:46:07 +0700

    Navbar test

--- frontend/src/components/navigation/NavBar/NavBar.jsx
+++ frontend/src/components/navigation/NavBar/NavBar.jsx
@@ -1,14 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Button, Nav, NavDropdown } from 'react-bootstrap';
 import { logout_api } from '../../backend_api/login';
 import usePersistentState from '../../../_helpers/UsePersistent';
 import useCustomFetch from '../../../_helpers/CustomFetchHook';
 
 export default function NavBar({userState}) {
     return (
-        <Navbar expand="lg">
-            <Container fluid className='Navbar' >
-                <SelectInfo userState={userState}></SelectInfo>
+        <Navbar expand="lg" bg='primary' className='bg-body-tetiary'>
+            <Container fluid>
+                <Navbar.Brand href='/'>NewsRss</Navbar.Brand>
+                <Navbar.Collapse id='navbar-colapse-login' className='justify-content-end'>
+                    <SelectInfo userState={userState}></SelectInfo>
+                </Navbar.Collapse>
                 {/* <Navbar.Brand href='/login'>I am home</Navbar.Brand> */}
             </Container>
         </Navbar>
@@ -22,10 +26,20 @@ function SelectInfo({userState}) {
     // console.log(username);
     // const is_logged = (username!="")?true:false;
     if (username!=="") {
-        return <div>
-            <p>Current_user: {username}</p>
-            <button onClick={logout}>Logout</button>
-        </div>
+        return <>
+            <Navbar.Text className='m-1'>
+                Logged as: {username}
+            </Navbar.Text>
+            <Button variant='secondary' onClick={logout}>
+                Logout
+            </Button>
+        </>
+        // return <div>
+        //     <p>Current_user: {username}</p>
+        //     <button onClick={logout}>Logout</button>
+        // </div>
     }
-    return (<a href='/login'>You must be logged</a>);
+    return <>
+        <Nav.Link href='/login'>Login</Nav.Link>
+    </>;
 }
